49a8fe1f4e02e75b767b7e25a439c6b76fddd058
Techmaster3000 <52240882+Techmaster3000@users.noreply.github.com>
2024-12-01 12:20:03 +0100

    startup

---
+++ CarAndAllReactASP.Server/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;    
+namespace CarAndAllReactASP.Server;
+
+public class User
+{
+    [Key]
+    public int KlantId { get; set; }
+    public string Naam { get; set; }
+    public string Adres { get; set; }
+    public string Email { get; set; }
+    //set the length to the usual length of a phone number
+    [MinLength(8)]
+    [MaxLength(15)]
+    public string Telefoonnr { get; set; }
+
+
+
+
+}
